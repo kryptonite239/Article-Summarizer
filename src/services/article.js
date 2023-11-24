b57544583ace2f69a467eee21a94de471eb1ccf0
Kailash Choudhary <74093176+kryptonite239@users.noreply.github.com>
2023-11-24 22:24:13 +0530

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -3,7 +3,7 @@ const apikey = "fbc6a953camsha0e8f2760e08195p10006cjsn40ee5710c732";
 export const articleApi = createApi({
   reducerPath: "articleApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize?url=https%3A%2F%2Ftime.com%2F6266679%2Fmusk-ai-open-letter%2F&length=3",
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com",
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
